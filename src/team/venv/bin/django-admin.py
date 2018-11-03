c70f5885a3fb667d744a5bfa449686919abbda8c
jxchen <jxchen31@gmail.com>
2018-11-03 16:01:07 -0400

    remove odd version of project

---
+++ src/team/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chenjiaxin/programmes/webgroup/Team17/src/team/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- src/teamproject/codereviewer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CodereviewerConfig(AppConfig):
-    name = 'codereviewer'
